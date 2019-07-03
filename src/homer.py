d8fcbe5f186032e92426701d484fc7ee3637b1e7
Waqas Younas <waqas.younas@gmail.com>
2019-07-03 16:19:02 +0500

    Edited the readme.md. Also, minor changes in the src/homer.py

--- src/homer.py
+++ src/homer.py
@@ -1,7 +1,15 @@
 """
+
+                 _   _
+                | | | | ___  _ __ ___   ___ _ __
+                | |_| |/ _ \| '_ ` _ \ / _ \ '__|
+                |  _  | (_) | | | | | |  __/ |
+                |_| |_|\___/|_| |_| |_|\___|_|
+
+
 Contains a class that can help run an analysis on a text (e.g. a blog post or an essay). You can get reading time,
 number of total sentences, readability scores (e.g. Flesch reading ease, Dale Chall readability scores), avg
-sentences per para, avg words per sentence, compulsive hedgers, zombie nouns, abstract nouns, and vague words.
+sentences per para, avg words per sentence, compulsive hedgers, zombie nouns, and vague words.
 
 Using this data you can make your text more clear, simple and useful for the reader.
 
@@ -43,12 +51,12 @@ class FleschReading(object):
         elif 90 <= self.score <= 100:
             return 'Very easy'
 
+
 class DaleChall(object):
     """https://en.wikipedia.org/wiki/Dale%E2%80%93Chall_readability_formula"""
 
     def __init__(self, text):
-        self.score =  round(textstat.dale_chall_readability_score(text), 1)
-        print("Score", self.score)
+        self.score = round(textstat.dale_chall_readability_score(text), 1)
 
     def grade(self):
         if self.score <= 4.9:
@@ -64,9 +72,16 @@ class DaleChall(object):
         elif self.score >= 9.0:
             return 'Average 13th or 15th grade student'
 
+
 class Word(object):
     """An abstraction of a 'word'. It categorizes whether a word is a compulsive hedger, an intensifier, abstract
-    noun or a vague word. """
+    noun or a vague word.
+    The idea to look for compulsive hedgers, intensifiers and abstract nouns came from Steven Pinker's book
+    `The Sense of Style: The Thinking Person's Guide to Writing in the 21st Century`.
+
+    Whereas the idea to look for vague words came after reading this paper:
+    https://litlab.stanford.edu/LiteraryLabPamphlet9.pdf
+    """
 
     def __init__(self, word):
         self.syllables = None
@@ -82,15 +97,14 @@ class Word(object):
         # Is it an abstract noun
         abs_nouns = ["approach", 'assumption', 'concept', 'condition', 'context', 'framework', 'issue', 'model',
                      'process', 'range', 'role', 'strategy', 'tendency', 'variable', 'perspective']
-        self.is_abstract_noun = True if any([self.word.startswith(abs_noun) for abs_noun in abs_nouns]) else False
         # Check if this word is a zombie noun
         zombies = ['ance', 'ment', 'ments', 'ation', 'ations', 'ing', 'ty']
-        # From world-bank paper: https://litlab.stanford.edu/LiteraryLabPamphlet9.pdf
+        # 'vague_words' is a combination of vague words and abstract nouns
         vague_words = ['accrual', 'derivative', 'fair value', 'portfolio', 'audit', 'poverty', 'evaluation',
                        'management', 'monitoring', 'effectiveness', 'performance', 'competitiveness',
                        'reform', 'assistance', 'growth', 'effort', 'capacity', 'transparency',
                        'effectiveness', 'progress', 'stability', 'protection', 'access',
-                       'implementation', 'sustainable']
+                       'implementation', 'sustainable'] + abs_nouns
 
         self.is_vague = True if self.word in vague_words else False
 
@@ -106,6 +120,7 @@ class Word(object):
 def get_word(word):
     return Word(word)
 
+
 class Sentence(object):
     """An abstraction that represents a sentence. We keep track of compulsive hedgers, intensifiers, abstract nouns,
     vague words and zombie nouns in a sentence. """
@@ -116,7 +131,6 @@ class Sentence(object):
         self.words = [get_word(word) for word in words]
         self.compulsive_hedgers = (word for word in self.words if word.is_compulsive_hedger)
         self.intensifiers = (word for word in self.words if word.is_intensifier)
-        self.abstract_nouns = (word for word in self.words if word.is_abstract_noun)
         self.vague_words = (word for word in self.words if word.is_vague)
         self.zombie_nouns = self.get_zombie_nouns()
         self.total_words = len(self.words)
@@ -126,9 +140,9 @@ class Sentence(object):
         return self.sentence
 
     def get_zombie_nouns(self):
-        """Get zombie nouns in a sentence. """
         words = nltk.word_tokenize(self.sentence)
         tags = nltk.pos_tag(words)
+        # zombies are verbs or adjectives that end with following
         zombies = ['ance', 'ment', 'ments', 'tion', 'tions', 'ism', 'ity']
         zombie_nouns = []
         for tag in tags:
@@ -150,11 +164,14 @@ class Paragraph(object):
         sentences = nltk.sent_tokenize(paragraph)
         self.sentences = [Sentence(sentence) for sentence in sentences]
         # Flattening list of lists
-        self.compulsive_hedgers = list(itertools.chain(*[sentence.compulsive_hedgers for sentence in self.sentences if sentence.compulsive_hedgers]))
-        self.intensifiers = list(itertools.chain(*[sentence.intensifiers for sentence in self.sentences if sentence.intensifiers]))
-        self.abstract_nouns = list(itertools.chain(*[sentence.abstract_nouns for sentence in self.sentences if sentence.abstract_nouns]))
-        self.zombie_nouns = list(itertools.chain(*[sentence.zombie_nouns for sentence in self.sentences if sentence.zombie_nouns]))
-        self.vague_words = list(itertools.chain(*[sentence.vague_words for sentence in self.sentences if sentence.vague_words]))
+        self.compulsive_hedgers = list(itertools.chain(*[sentence.compulsive_hedgers for sentence in self.sentences
+                                                         if sentence.compulsive_hedgers]))
+        self.intensifiers = list(itertools.chain(*[sentence.intensifiers for sentence in self.sentences
+                                                   if sentence.intensifiers]))
+        self.zombie_nouns = list(itertools.chain(*[sentence.zombie_nouns for sentence in self.sentences
+                                                   if sentence.zombie_nouns]))
+        self.vague_words = list(itertools.chain(*[sentence.vague_words for sentence in self.sentences
+                                                  if sentence.vague_words]))
         self.flesch_reading_score = FleschReading(paragraph).grade()
         self.dale_chall_readability_score = DaleChall(paragraph).grade()
         self.total_words = sum([sentence.total_words for sentence in self.sentences])
@@ -162,15 +179,18 @@ class Paragraph(object):
         self.total_and_words = sum([sentence.total_and_words for sentence in self.sentences if sentence.total_and_words])
         self.avg_words_per_sentence = round(self.total_words/ self.total_sentences, 2)
         self.longest_sentence = max(sentences)
-        # TODO: a)- highlight longer sentences b)- highlight long paras
 
     def __repr__(self):
         return "sentences= {sentences}, flesch_reading_score={flesch_reading}, dale_chall={dale_chall}".format(
-            sentences=self.total_sentences, flesch_reading=self.flesch_reading_score, dale_chall=self.dale_chall_readability_score
-        )
+            sentences=self.total_sentences, flesch_reading=self.flesch_reading_score,
+            dale_chall=self.dale_chall_readability_score)
+
+
 class Article(object):
     """
-    An article represents a block of tests consisting of paragraphs. For example, it could be an essay or a blog post.
+    An article represents a block of tests, i.e. an essay or an article, consisting of paragraphs. For example, it could
+    be an essay or a blog post.
+
     This class keeps track of paragraphs, total sentences, Flesch reading ease, Dale Chall readability score, avg
     sentences per para, avg words per sentence, compulsive hedgers, zombie nouns, abstract nouns, and vague words.
     """
@@ -187,28 +207,28 @@ class Article(object):
         self.total_sentences = sum([paragraph.total_sentences for paragraph in self.paragraphs])
         self.flesch_reading_score = FleschReading(self.text).grade()
         self.dale_chall_readability_score = DaleChall(self.text).grade()
-        self.read_time = self.total_words / 200 # gets us minutes (since we read 200 words per minute so 600 / 200 is 3 minutes of reading time)
+        self.read_time = self.total_words / 200 # gets us minutes (since we read 200 words per minute so 600 / 200 is 3
+        # minutes of reading time)
         self.avg_sentences_per_para = round(self.total_sentences / self.total_paragrpahs, 2)
         self.avg_words_per_sentence = round(self.total_words / self.total_sentences, 2)
         # Flattening list of lists
-        self.zombie_nouns = list(itertools.chain(*(paragraph.zombie_nouns for paragraph in self.paragraphs if paragraph.zombie_nouns)))
+        self.zombie_nouns = list(itertools.chain(*(paragraph.zombie_nouns for paragraph in self.paragraphs
+                                                   if paragraph.zombie_nouns)))
         self.total_zombie_nouns = len(self.zombie_nouns)
-        self.compulsive_hedgers = list(itertools.chain(*(paragraph.compulsive_hedgers for paragraph in self.paragraphs if paragraph.compulsive_hedgers)))
+        self.compulsive_hedgers = list(itertools.chain(*(paragraph.compulsive_hedgers for paragraph in self.paragraphs
+                                                         if paragraph.compulsive_hedgers)))
         self.total_compulsive_hedgers = len(self.compulsive_hedgers)
-        self.intensifiers = list(itertools.chain(*(paragraph.intensifiers for paragraph in self.paragraphs if paragraph.intensifiers)))
+        self.intensifiers = list(itertools.chain(*(paragraph.intensifiers for paragraph in self.paragraphs
+                                                   if paragraph.intensifiers)))
         self.total_intensifiers = len(self.intensifiers)
-        self.abstract_nouns = list(itertools.chain(*(paragraph.abstract_nouns for paragraph in self.paragraphs if paragraph.abstract_nouns)))
-        self.total_abstract_nouns = len(self.abstract_nouns)
-        self.vague_words = list(itertools.chain(*(paragraph.vague_words for paragraph in self.paragraphs if paragraph.vague_words)))
+        self.vague_words = list(itertools.chain(*(paragraph.vague_words for paragraph in self.paragraphs
+                                                  if paragraph.vague_words)))
         self.total_vague_words = len(self.vague_words)
         self.and_words = [paragraph.total_and_words for paragraph in self.paragraphs if paragraph.total_and_words]
         self.total_and_words = sum(self.and_words)
         self.and_frequency = str(round(self.total_and_words / self.total_words * 100, 2)) + " %"
-        print("Article score")
-        print("-------")
-        print(self.dale_chall_readability_score)
 
-    def words_with_most_syllables(self):
+    def ten_words_with_most_syllables(self):
         """This gets us 10 words with most syllables in a text"""
         d = cmudict.dict()
         words = nltk.word_tokenize(self.text)
@@ -237,19 +257,19 @@ class Article(object):
             [Color('{autocyan}Paragraph Stats{/autocyan}')],
             ['Paragraph #', '']
         ]
-        print("Paragraph scores")
         for item, para in enumerate(self.paragraphs):
             sentences = Color('{red}%s{/red}' % str(para.total_sentences)) if para.total_sentences > 5 else str(para.total_sentences)
             avg_words_per_sentence = Color(
                 '{red}%s{/red}' % str(para.avg_words_per_sentence)) if para.avg_words_per_sentence > 25 else str(
                 para.avg_words_per_sentence)
             table_data.append([item + 1,
-                               '{sentences} {sent_tag}. {words} {word_tag}. {avg_words} {avg_word_tag}. "{longest_sent}..." is the {long_tag} sentence.'.format(
+                               '{sentences} {sent_tag}. {words} {word_tag}. {avg_words} {avg_word_tag}. '
+                               '"{longest_sent}..." is the {long_tag} sentence.'.format(
                                    sentences=sentences, sent_tag=sentence_tag, words=para.total_words,
                                    word_tag=word_tag, avg_words=avg_words_per_sentence, avg_word_tag=avg_word_tag,
                                    longest_sent=para.longest_sentence[0:10], long_tag=long_tag
                                )])
-            table_data.append(["" , "Flesh Reading score={flesch_reading}, Dale Chall Readability= {dale_chall}".format(
+            table_data.append(["", "Flesh Reading score={flesch_reading}, Dale Chall Readability= {dale_chall}".format(
                 flesch_reading=para.flesch_reading_score, dale_chall=para.dale_chall_readability_score
             )])
 
@@ -261,6 +281,15 @@ class Article(object):
 
     def print_article_stats(self):
         """This method is called to present article stats on a command line."""
+        banner = """
+                 _   _
+                | | | | ___  _ __ ___   ___ _ __
+                | |_| |/ _ \| '_ ` _ \ / _ \ '__|
+                |  _  | (_) | | | | | |  __/ |
+                |_| |_|\___/|_| |_| |_|\___|_|
+
+        """
+        # print(banner)
         table_data = [
             [Color('{autocyan}Overall Stats{/autocyan}')],
             ['Reading time (in mins)', self.read_time],
@@ -275,7 +304,6 @@ class Article(object):
             ['"and" frequency"', self.and_frequency],
             ['Compulsive Hedgers', self.total_compulsive_hedgers],
             ['Intensifiers', self.total_intensifiers],
-            ['Abstract Nouns', self.total_abstract_nouns],
             ['Vague words', self.total_vague_words],
 
         ]
@@ -296,23 +324,21 @@ class Article(object):
             print(Color(msg))
 
         if len(self.intensifiers) >= 1:
-            msg = '{red} **- Intensifiers: %s {/red}\r\n' % ', '.join(str(intensifier) for intensifier in self.intensifiers)
-            print(Color(msg))
-
-        if len(self.abstract_nouns) >= 1:
-            msg = '{red} **- Abstract nouns: %s {/red}\r\n' % ', '.join(str(abs_noun) for abs_noun in self.abstract_nouns)
+            msg = '{red} **- Intensifiers: %s {/red}\r\n' % ', '.join(str(intensifier) for intensifier
+                                                                      in self.intensifiers)
             print(Color(msg))
 
         if len(self.vague_words) >= 1:
             msg = '{red} **- Vague words: %s {/red}\r\n' % ', '.join(str(vague_word) for vague_word in self.vague_words)
             print(Color(msg))
 
-        words_with_most_syllables = self.words_with_most_syllables()
-        if len(words_with_most_syllables) >= 1:
-            msg = '{red} **- 10 words with most syllables: %s {/red}\r\n' % ', '.join(words_with_most_syllables)
+        ten_words_with_most_syllables = self.ten_words_with_most_syllables()
+        if len(ten_words_with_most_syllables) >= 1:
+            msg = '{red} **- 10 words with most syllables: %s {/red}\r\n' % ', '.join(ten_words_with_most_syllables)
             print(Color(msg))
 
-        msg = "{red} **- Twenty most repeated words (highest to lowest): %s {/red}\r\n" % ', '.join(self.get_n_most_repeated_words(20))
+        msg = "{red} **- Twenty most repeated words (highest to lowest): %s {/red}\r\n" % \
+              ', '.join(self.get_n_most_repeated_words(20))
         print(Color(msg))
 
     def get_n_most_repeated_words(self, n):
@@ -322,7 +348,7 @@ class Article(object):
         all_words_except_stop = nltk.FreqDist(w.lower() for w in words if w[0].isalpha() and w not in stopwords)
         return [word for word, freq in all_words_except_stop.most_common(n)]
 
-article = Article('name', 'author', open('/Users/waqas/PycharmProjects/sense_of_style/experiment/oped.txt').read())
+article = Article('name', 'author', open('/Users/waqas/code/homer/experiment/oped.txt').read())
 # article = Article('name', 'author', open('/Users/waqas/PycharmProjects/sense_of_style/experiment/economic_survey_pk_2018_19/overview_of_economy.txt').read())
 article.print_article_stats()
 # # print(article.words_with_most_syllables())
